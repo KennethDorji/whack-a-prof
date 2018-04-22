0587cc4ef66a21df216b3d1cab5b289610e1ef19
Xavier Ma <xavma@its.brooklyn.cuny.edu>
2018-04-22 23:08:55 +0000

    Adding hud.js, to be build upon for future commits
    git-svn-id: svn+ssh://146.245.252.151/users1/st/sheliu/group2repo@98 eba39273-bf89-409f-9094-0b381e5c0774

---
+++ src/hud.js
@@ -0,0 +1,58 @@
+/*
+*	hud.js
+*	Heads-Up-Display
+*	CLass that will implement Whack-A-Prof's Overlay
+*	Score/Point System, Timer, Pause button(Maybe?)
+*	Counter for each "Mole hit" to caclulate score
+*/
+
+
+class Hud extends Layer{
+	/*variable for pause button to be intially false
+	//Pause Button work in progress
+	var paused = false;
+	function gamePaused()
+	{
+		if(!paused)
+		{
+			paused = true;
+		}
+		else if(paused)
+		{
+			paused = false;
+		}
+	}
+	window.addEventListener('keydown',function(e))
+	{
+		//Char codes to identify Pause button, 'p' key is == 80
+		var key = e.keyCode;
+		if(key == 80)
+		{
+			gamePaused();
+		}
+	}
+	*/
+
+	function scoreCounter()
+	{
+		//Function for score counter
+		//Based on specs, Trustees: 10	Professors: 5	Department Head: 8
+
+
+
+	}
+
+	function clockTimer()
+	{
+		//Function for Timer ticking down in Whack-A-Prof
+
+		
+	}
+
+
+
+
+
+
+
+}
