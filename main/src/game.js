1ae6ab7e80e2692c3d4cf02fb0dd072ea1937c42
wolfchimneyrock <tholos@gmail.com>
2018-05-07 22:30:32 +0000

    add background
    git-svn-id: svn+ssh://146.245.252.151/users1/st/sheliu/group2repo@139 eba39273-bf89-409f-9094-0b381e5c0774

--- main/src/game.js
+++ main/src/game.js
@@ -50,6 +50,7 @@ class Game extends Layer {
       });
     let self = this;
     self.maxTime = options.maxTime || 60;
+    self.maxGrass = options.maxGrass || 7;
     self.startTime = null;
     self.hitRadius = HITRADIUS * L.overallScale;
     self.cast = new Cast();
@@ -68,7 +69,9 @@ class Game extends Layer {
             return Promise.all([
                 self.cast.init(container),
                 ...self.holes.map(hole => hole.init(container)),
-                Util.loadImage('sprites/portal.svg').then(image => self.portal = image),
+                Util.loadImage('sprites/table.svg').then(image => self.portal = image),
+                Util.loadImage('sprites/background.svg').then(image => self.background = image),
+                Util.loadImage('sprites/grass.svg').then(image => self.grass = image),
                 //Util.loadImage('sprites/table.svg').then(image => self.table = image),
                 self.music.load()
             ]);
@@ -83,22 +86,66 @@ class Game extends Layer {
       self.holes.forEach(hole => {
           self.ctx.save();
           self.ctx.translate(
-              hole.coordinate.x - this.offset.x - 40*L.overallScale, 
-              hole.coordinate.y + hole.size - this.offset.y - 100*L.overallScale
+              hole.coordinate.x - this.offset.x, // - 40*L.overallScale, 
+              hole.coordinate.y + hole.size - this.offset.y // - 100*L.overallScale
           );
           self.ctx.scale(L.overallScale, L.overallScale); 
           self.ctx.drawImage(self.portal, 0, 0);
           self.ctx.restore();
 
           // temporarily draw "hit circles"
+          /*
+
           self.ctx.beginPath();
           self.ctx.arc(hole.hitCenter.x, hole.hitCenter.y, self.hitRadius, 0, 2*Math.PI);
           self.ctx.closePath();
           self.ctx.stroke();
+          */
       });
 
   }
-  
+
+  drawBackground() {
+      let self = this;
+      let ratio = (self.background.width-60) / (self.background.height);
+      let radius = self.canvas.width / (2*ratio);
+
+      // draw the sky
+      let skyGrad = self.ctx.createRadialGradient(
+              self.canvas.width/2, radius, 0, 
+              self.canvas.width/2, radius, radius*2);
+      skyGrad.addColorStop(0, "#99ccff");
+      skyGrad.addColorStop(0.5, "#5599ee");
+      skyGrad.addColorStop(1, "#3377bb");
+      self.ctx.fillStyle = skyGrad;
+      self.ctx.fillRect(0, 0, self.canvas.width, self.canvas.width / ratio);
+
+      // draw the library
+      self.ctx.drawImage(self.background, 
+              150, 40/ratio,    // sX, sY
+              self.background.width-300, self.background.height-175/ratio,    // sW, sH
+              0, 0,    // dX, dY
+              self.canvas.width, self.canvas.width / ratio     // dW, dH
+              );
+
+      // draw blades of grass
+
+      self.ctx.globalCompositeOperation = "color-burn";
+      let ystart = -self.grass.height/2 + self.canvas.width / ratio;
+      let ydelta = (self.canvas.height - ystart) / self.maxGrass;
+      let ypos;
+      let xpos;
+      self.ctx.fillStyle = "#8cc53e";
+      for (ypos = ystart; ypos < self.canvas.height; ypos += ydelta) {
+          let xofs = -Util.uniform(self.grass.width);
+          for (xpos = 0; xpos + xofs< self.canvas.width; xpos += self.grass.width) {
+              self.ctx.drawImage(self.grass, xpos + xofs, ypos);
+          }
+          self.ctx.fillRect(0, ypos + self.grass.height - 1, self.canvas.width, self.canvas.height);
+      }
+
+      self.ctx.globalCompositeOperation = "source-over";
+  }  
 
   checkHit(loc) {
       let self = this;
@@ -155,6 +202,7 @@ class Game extends Layer {
       currentState = States.PLAYING;
       let self = this;
       self.startTime = window.performance.now();
+      self.drawBackground();
       self.drawHoles();
       Promise.all([
               L.game.fadeIn(),
